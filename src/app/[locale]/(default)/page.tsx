53d3b2b94888c7d129a1d277da195b0570f6b511
15869689963@163.com <15869689963@163.com>
2025-06-24 10:31:27 +0800

    多语言优化

--- src/app/[locale]/(default)/page.tsx
+++ src/app/[locale]/(default)/page.tsx
@@ -51,6 +51,7 @@ export default async function LandingPage({
       <Generator />
       <Wallpapers wallpapers={wallpapers} /> 
        {page.feature && <Feature section={page.feature} />}
+       {page.pricing && <Pricing pricing={page.pricing} /> }
       {/* {page.branding && <Branding section={page.branding} />}
       {page.introduce && <Feature1 section={page.introduce} />}
       {page.benefit && <Feature2 section={page.benefit} />}
